580739c2bf451bc3174bd178bc442c914df57e24
Saurabh <saurabh@pep.live>
2022-06-13 18:54:44 +0530

    Stable

--- components/Shared/EmptyState.tsx
+++ components/Shared/EmptyState.tsx
@@ -12,7 +12,7 @@ const EmptyState: React.FC<EmptyStateInterface> = ({ title, message, children })
   return (
     <div className="text-center flex min-h-free justify-center items-center">
       <div>
-        <LottieAnimation animationData={emptyLottie} height={200} width={200} loop={false} />
+        <LottieAnimation animationData={emptyLottie} height={140} width={140} loop={false} />
         <h3 className="text-base font-medium text-slate-400">{title}</h3>
         <p className="mt-1 text-sm text-slate-300">{message}</p>
       </div>
