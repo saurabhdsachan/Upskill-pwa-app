1e12c5d73ae200edfa54959b7a5fed0deab5cee6
Saurabh <saurabh@pep.live>
2022-06-23 15:53:35 +0530

    WIP

--- pages/success.tsx
+++ pages/success.tsx
@@ -1,4 +1,5 @@
 import Layout from '@components/Shared/Layout';
+import LoadingState from '@components/Shared/LoadingState';
 import LottieAnimation from '@components/Shared/LottieAnimation';
 import successLottie from '@public/lotties/success.json';
 import { paymentVerify } from '@utils/apiData';
@@ -6,6 +7,7 @@ import Head from 'next/head';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React, { useCallback, useEffect, useState } from 'react';
+import { Else, If, Then } from 'react-if';
 
 const Slots: React.FC = () => {
   const [bookingDetails, setBookingDetails] = useState({
@@ -44,17 +46,31 @@ const Slots: React.FC = () => {
       </Head>
       <Layout.Header title="Booking Success" backflow={false} showShare={false} showBooking={false} />
       <Layout.Body>
-        <div className="bg-white items-center justify-center">
-          <LottieAnimation animationData={successLottie} loop={false} width={240} height={240} />
-        </div>
-        <div className="px-6 text-center">
-          <p className="my-6">Your booking has been confirmed</p>
-          <Link href="/bookings/booked?type=today">
-            <a className="inline-flex items-center justify-center py-2 px-6 border border-transparent rounded-lg text-sm font-medium text-white bg-gradient-to-r from-gray-900 to-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-gray-400">
-              My Bookings
-            </a>
-          </Link>
-        </div>
+        <If condition={loading}>
+          <Then>
+            <LoadingState />
+          </Then>
+          <Else>
+            <div className="bg-white items-center justify-center">
+              <LottieAnimation animationData={successLottie} loop={false} width={240} height={240} />
+            </div>
+            <div className="px-6 text-center">
+              <p className="my-6">Your booking has been confirmed</p>
+              <div className="my-6">
+                <h3 className="text-xl">{bookingDetails?.label}</h3>
+                {bookingDetails?.paymentStatus && (
+                  <p className="text-slate-400">Status: {bookingDetails?.paymentStatus}</p>
+                )}
+                <p className="text-slate-400">{bookingDetails?.priceLabel}</p>
+              </div>
+              <Link href="/bookings/booked?type=today">
+                <a className="inline-flex items-center justify-center py-2 px-6 border border-transparent rounded-lg text-sm font-medium text-white bg-gradient-to-r from-gray-900 to-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-1 focus:ring-offset-2 focus:ring-gray-400">
+                  My Bookings
+                </a>
+              </Link>
+            </div>
+          </Else>
+        </If>
       </Layout.Body>
     </Layout>
   );
